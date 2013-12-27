8aadb82b588a980246e3120127be1c777a15aef4
xdxttt <xdxttt@gmail.com>
2013-12-27 17:57:04 +0800

    add

---
+++ Assets/Scripts/CameraPoint.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPoint : MonoBehaviour {
+
+	public static CameraPoint Instance = null;
+	
+	void Awake()
+	{
+		Instance = this;
+	}	
+}
---
+++ Assets/Scripts/GridMap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridMap : MonoBehaviour {
+
+	void Start () {
+	}
+	void OnDrawGizmos()
+	{
+		if(GameCamera.Inst)
+		{
+			Debug.Log("OnDrawGizmos ");
+
+			if(GameCamera.Inst.selectGameObj){
+				Gizmos.color = Color.blue;
+				float height = 1;
+				for(int i = 0;i<200;i++){
+					Gizmos.DrawLine(new Vector3(i,height,0),new Vector3(i,height,200));
+				}
+				for(int i = 0;i<200;i++){
+					Gizmos.DrawLine(new Vector3(0,height,i),new Vector3(200,height,i));
+				}
+			}
+		}
+	}
+
+}
